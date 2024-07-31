1281dfa8514a040d1922f0f844ba0036ddcf71f9
daback <daback93@gmail.com>
2024-07-31 09:32:21 +0900

    Add: initialize upload

---
+++ src/main/java/com/cdls/bottletalk/BottletalkApplication.java
@@ -0,0 +1,13 @@
+package com.cdls.bottletalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottletalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottletalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdls/bottletalk/BottletalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdls.bottletalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottletalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
